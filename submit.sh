9dc35b104ff0d0c1c8642f108855ea86afe075b8
Paul Anderson <pauleanderson@gmail.com>
2022-10-04 12:00:48 -0700

    update

--- submit.sh
+++ submit.sh
@@ -1,1 +1,2 @@
-sed -i s/ubuntu-latest/self-hosted/g .github/workflows/classroom.yml && git add . && git commit -m submission && git push
+[ -d ".github" ] && sed -i s/ubuntu-latest/self-hosted/g .github/workflows/classroom.yml 
+git add . && git commit -m submission && git push
